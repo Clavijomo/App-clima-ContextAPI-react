da9a5f10e719feb3feccc8a2c08251a781dfe86b
Jonathan Clavijo <jonathanclavijo0907@gmail.com>
2023-03-30 18:52:20 -0500

    :rocket: Finalizacion del proyecto de API de clima usando Context API y demas funciones de React

---
+++ src/hooks/useClima.jsx
@@ -0,0 +1,8 @@
+import {useContext} from 'react'
+import ClimaContext from '../context/ClimaProvider'
+
+const useClima = () => {
+    return useContext(ClimaContext);
+}
+
+export default useClima;
